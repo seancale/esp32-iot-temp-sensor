471344b54dbe79a5f5a8317cec51bdb0ad5bc749
Sean <90122919+seancale@users.noreply.github.com>
2022-01-23 21:24:52 -0600

    Serial command line with wifi commands

--- main/main.c
+++ main/main.c
@@ -2,11 +2,45 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "lib/dht22/include/DHT.h"
+#include "esp_console.h"
+#include "command_wifisearch.c"
+#include "nvs_flash.h"
 
 void app_main(void) {
+    //NVS init
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    //Console init
+    esp_console_repl_t* repl = NULL;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    repl_config.prompt = "sensor>";
+
+#if CONFIG_ESP_CONSOLE_UART
+    esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&uart_config, &repl_config, &repl));
+#elif CONFIG_ESP_CONSOLE_USB_CDC
+    esp_console_dev_usb_cdc_config_t cdc_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&cdc_config, &repl_config, &repl));
+#elif CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG
+    esp_console_dev_usb_serial_jtag_config_t usbjtag_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&usbjtag_config, &repl_config, &repl));
+#endif
+
+    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
+    //Register commands
+    register_wifisearch();
+
+    //DHT22 init
     setDHTgpio(4);
 
-    while (1) {
+    //Main loop
+    /*while (1) {
         printf("Reading DHT...\n");
         
         int ret = readDHT();
@@ -14,5 +48,5 @@ void app_main(void) {
         printf("Hum: %.1f Tmp: %.1f\n", getHumidity(), getTemperature());
 
         vTaskDelay(3000 / portTICK_RATE_MS);
-    }
+    }*/
 }
